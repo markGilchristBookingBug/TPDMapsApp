83347a21817435f5534a97654483f4caae0eb1ef
markGilchristBookingBug <mark@mark-gilchrists-mac-mini.local>
2016-06-01 08:37:46 +0100

    - added the typedef to the enum that was causing build errors

--- TPDMapsApp/TPDMapsApp/TPDMapsApp.h
+++ TPDMapsApp/TPDMapsApp/TPDMapsApp.h
@@ -11,7 +11,7 @@
  The mode of travel requested.
  */
 FOUNDATION_EXPORT NSInteger TPDMapsAppTravelMode;
-NS_ENUM(NSInteger, TPDMapsAppTravelMode) {
+typedef NS_ENUM(NSInteger, TPDMapsAppTravelMode) {
     TPDMapsAppTravelModeNone = 0,
     TPDMapsAppTravelModeDriving = 1,
     TPDMapsAppTravelModeWalking = 2,
